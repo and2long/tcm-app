ca83d3a6d9f35ff0224cc27c3b74303f469341d8
Lilong Zhang <and2long@gmail.com>
2025-02-12 19:48:06 +0800

    弹出错误信息

--- lib/core/blocs/handle_error.dart
+++ lib/core/blocs/handle_error.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_ytlog/log.dart';
 import 'package:tcm/utils/toast_util.dart';
 
@@ -5,5 +6,14 @@ const String _tag = 'HandleError';
 void handleError(Object e, {StackTrace? stackTrace}) {
   Log.e(_tag, e);
   Log.e(_tag, stackTrace);
+  if (e is DioException) {
+    if (e.response?.statusCode == 400) {
+      final errorMessage = e.response?.data['error'];
+      if (errorMessage != null) {
+        ToastUtil.show(errorMessage);
+        return;
+      }
+    }
+  }
   ToastUtil.show(e.toString());
 }
