bbfca744c27c86dd43b9ee1b26128848623bd42b
Lilong Zhang <and2long@gmail.com>
2025-02-15 09:38:53 +0800

    限制程序字体大小不受系统字体大小影响

--- lib/main.dart
+++ lib/main.dart
@@ -41,39 +41,46 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<LocaleStore>(
-        builder: (BuildContext context, LocaleStore value, Widget? child) {
-      return Consumer<AppProvider>(
-        builder: (context, provider, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            navigatorKey: MyApp.navigatorKey,
-            onGenerateTitle: (context) => S.appName,
-            theme: AppTheme.light,
-            darkTheme: AppTheme.dark,
-            themeMode: provider.themeMode,
-            localizationsDelegates: const [
-              GlobalMaterialLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              // 项目本地化资源代理
-              S.delegate,
-            ],
-            // 支持的语言
-            supportedLocales: S.supportedLocales,
-            locale: Locale(value.languageCode),
-            home: SPUtil.getIsDoctor() ? const HomePage() : const OperatePage(),
-            navigatorObservers: [MyRouteObserver()],
-            // builder: (context, child) => GestureDetector(
-            //   onTap: () => CommonUtil.hideKeyboard(context),
-            // ),
-            builder: FlutterSmartDialog.init(
-              loadingBuilder: (String msg) => CustomLoadingWidget(msg: msg),
-            ),
-          );
-        },
-      );
-    });
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(
+        // 程序的字体大小不受系统字体大小影响
+        textScaler: TextScaler.noScaling,
+      ),
+      child: Consumer<LocaleStore>(
+          builder: (BuildContext context, LocaleStore value, Widget? child) {
+        return Consumer<AppProvider>(
+          builder: (context, provider, child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              navigatorKey: MyApp.navigatorKey,
+              onGenerateTitle: (context) => S.appName,
+              theme: AppTheme.light,
+              darkTheme: AppTheme.dark,
+              themeMode: provider.themeMode,
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                // 项目本地化资源代理
+                S.delegate,
+              ],
+              // 支持的语言
+              supportedLocales: S.supportedLocales,
+              locale: Locale(value.languageCode),
+              home:
+                  SPUtil.getIsDoctor() ? const HomePage() : const OperatePage(),
+              navigatorObservers: [MyRouteObserver()],
+              // builder: (context, child) => GestureDetector(
+              //   onTap: () => CommonUtil.hideKeyboard(context),
+              // ),
+              builder: FlutterSmartDialog.init(
+                loadingBuilder: (String msg) => CustomLoadingWidget(msg: msg),
+              ),
+            );
+          },
+        );
+      }),
+    );
   }
 }
 
